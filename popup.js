2dcaa4da675e2a86172e9dc3ad96e20b92dde952
Itamar <itamar.olmert@gmail.com>
2022-04-17 04:50:22 +0300

    cleanup

--- popup.js
+++
@@ -1,7 +0,0 @@
-function urlToTAU(url) {
-    return "".concat(url.origin.replace(".", "-"), ".rproxy.tau.ac.il").concat(url.pathname).concat(url.search).concat(url.hash);
-}
-function redirecTtoTAU() {
-    location.href = urlToTAU(window.location);
-}
-document.getElementById("tauer").addEventListener("click", redirecTtoTAU);
