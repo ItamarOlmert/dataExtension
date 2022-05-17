ff30b0bd4f5942cae8ce91a785a790ff735704c7
Itamar Olmert <itamar.olmert@gmail.com>
2022-05-17 20:54:54 +0300

    Added cross-browser support

--- background.js
+++
@@ -1,7 +0,0 @@
-function urlToTAU(url) {
-    return "".concat(url.origin.replace(/\./g, "-"), ".rproxy.tau.ac.il").concat(url.pathname).concat(url.search).concat(url.hash);
-}
-chrome.action.onClicked.addListener(function (tab) {
-    var currentPage = new URL(tab.url);
-    chrome.tabs.create({ url: urlToTAU(currentPage) });
-});
--- background.ts
+++ background.ts
@@ -2,7 +2,7 @@ function urlToTAU(url: URL): string {
     return `${url.origin.replace(/\./g, "-")}.rproxy.tau.ac.il${url.pathname}${url.search}${url.hash}`;
 }
 
-chrome.action.onClicked.addListener((tab) => {
-    let currentPage = new URL(tab.url);
-    chrome.tabs.create({url: urlToTAU(currentPage)});
-})
+browser.action.onClicked.addListener((tab) => {
+    let currentPage = new URL(tab.url!!);
+    browser.tabs.create({ url: urlToTAU(currentPage) });
+});
