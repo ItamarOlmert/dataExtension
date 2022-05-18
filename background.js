3afb2117a1f5dc3facd4c6fbc168aa600632604a
Itamar Olmert <itamar.olmert@gmail.com>
2022-05-18 10:31:14 +0300

    now deprecated on chrome, but works on FF. + icon.

---
+++ background.js
@@ -0,0 +1,7 @@
+function urlToTAU(url) {
+    return "".concat(url.origin.replace(/\./g, "-"), ".rproxy.tau.ac.il").concat(url.pathname).concat(url.search).concat(url.hash);
+}
+chrome.browserAction.onClicked.addListener(function (tab) {
+    var currentPage = new URL(tab.url);
+    chrome.tabs.create({ url: urlToTAU(currentPage) });
+});
